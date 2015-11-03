4174edc442042fb98843efc88550a4cb20facdcc
VORALENT <miyamoto@voralent.com>
2015-11-03 14:18:50 +0900

    initial commit

---
+++ UWP-RichEditBox-Search-Sample/MainPage.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWP_RichEditBox_Search_Sample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        private bool _formatChanged = false;
+        private string _oldQuery = string.Empty;
+        private Color _highLihgtColor = Color.FromArgb(255, 150, 190, 255);
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void txtContent_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Loading Sample File
+            var sampleFile = await Windows.Storage.StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///SampleText/SampleTextFile.txt"));
+            using (Stream st = (await sampleFile.OpenReadAsync()).AsStream())
+            using (TextReader reader = new StreamReader(st))
+            {
+                this.txtContent.Document.SetText(Windows.UI.Text.TextSetOptions.None, await reader.ReadToEndAsync());
+            }
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            string query = txtSearchBox.Text;
+            FindText(query, true);
+        }
+
+
+        private void FindText(string query, bool Isfocus = false)
+        {
+            if (string.IsNullOrEmpty(query))
+                return;
+
+            if (!_oldQuery.Equals(query))
+                ResetTextFormat();
+
+            _oldQuery = query;
+            string docText;
+            txtContent.Document.GetText(Windows.UI.Text.TextGetOptions.None, out docText);
+            var start = txtContent.Document.Selection.EndPosition;
+            var end = docText.Length;
+            var range = txtContent.Document.GetRange(start, end);
+            int result = range.FindText(query, end - start, Windows.UI.Text.FindOptions.None);
+
+            if (result == 0)
+            {
+                txtContent.Document.Selection.SetRange(0, 0);
+            }
+            else
+            {
+                txtContent.Document.Selection.SetRange(range.StartPosition, range.EndPosition);
+                range.CharacterFormat.BackgroundColor = _highLihgtColor;
+                _formatChanged = true;
+                range.ScrollIntoView(Windows.UI.Text.PointOptions.None);
+                if (Isfocus)
+                    txtContent.Focus(FocusState.Keyboard);
+            }
+        }
+
+        private void ResetTextFormat()
+        {
+            if (_formatChanged)
+            {
+                // Very high cost process
+                string docText;
+                txtContent.Document.GetText(Windows.UI.Text.TextGetOptions.None, out docText);
+                var range = txtContent.Document.GetRange(0, docText.Length);
+                range.CharacterFormat.BackgroundColor = ((SolidColorBrush)txtContent.Background).Color;
+                _formatChanged = false;
+            }
+        }
+
+        private void txtSearchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txtSearchBox.Text.Length == 0)
+                ResetTextFormat();
+        }
+
+        private void txtSearchBox_KeyUp(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+            {
+                string query = txtSearchBox.Text;
+                FindText(query);
+            }
+        }
+    }
+}
